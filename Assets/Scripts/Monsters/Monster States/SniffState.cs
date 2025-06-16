95a97bbbdb08862f0f21e1442dc239ba432ab9a8
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-16 17:43:57 +0900

    feat: sniff 상태 추가
    - 추후 creep 몬스터 추가 시 사용할 클래스

---
+++ Assets/Scripts/Monsters/Monster States/SniffState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniffState : State
+{
+    public SniffState(BaseMonster baseMonster) : base(baseMonster) { }
+
+    public override void Enter()
+    {
+    }
+    public override void Execute()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
