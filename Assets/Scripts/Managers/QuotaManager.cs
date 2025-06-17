7d908dc098c89c74fd7fc73ce21bb348e8dddba9
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-17 17:12:02 +0900

    fix: 할당량 표기 기능 분리
    - 여러 씬을 오가는 싱글톤 QuotaManager가 텍스트를 직렬화해서 가지지 않고 데이터 컨테이너로써만 동작하도록 변경

--- Assets/Scripts/Managers/QuotaManager.cs
+++ Assets/Scripts/Managers/QuotaManager.cs
@@ -8,59 +8,46 @@ public class QuotaManager : MonoBehaviour
 {
     private StageManager _stageManager;
 
-    private int _requiredQuota;
-    public int RequiredQuota { get { return _requiredQuota; } }
-    private int _currentQuota;
-    public int CurrentQuota { get { return _currentQuota; } }
-    private int _randomRange => (int)(_requiredQuota * 0.1f);
+    public ObservableProperty<int> CurrentQuota = new(0);
+    public ObservableProperty<int> RequiredQuota = new(0);
 
-    [SerializeField] private TMP_Text _quotaText;
-
-    private StringBuilder _sb = new();
-
-
-    //private void Start()
-    //{
-    //    GameManager.Instance.StageManager.CurrentStage.Subscribe(SetRequiredQuota);
-    //    GameManager.Instance.StageManager.CurrentStage.Subscribe(RenewQuotaWhenStageChanged);
-    //}
+    private int _randomRange => (int)(RequiredQuota.Value * 0.1f);
 
     private void OnDestroy()
     {
         GameManager.Instance.StageManager.CurrentStage.Unsubscribe(SetRequiredQuota);
-        GameManager.Instance.StageManager.CurrentStage.Unsubscribe(RenewQuotaWhenStageChanged);
     }
 
     public void Init(StageManager stageManager)
     {
         _stageManager = stageManager;
         _stageManager.CurrentStage.Subscribe(SetRequiredQuota);
-        _stageManager.CurrentStage.Subscribe(RenewQuotaWhenStageChanged);
-    }
 
-    private void RenewQuotaWhenStageChanged()
-    {
-        _sb.Clear();
-
-        _sb.Append(_currentQuota);
-        _sb.Append("$ / ");
-        _sb.Append(_requiredQuota);
-        _sb.Append("$");
-
-        _quotaText.text = _sb.ToString();
+        SetCurrentQuotaToZero();
+        SetRequiredQuota();
     }
 
     private void SetRequiredQuota()
     {
-        _requiredQuota = 5000 * GameManager.Instance.StageManager.CurrentStage.Value;
-        int random = Random.Range(_requiredQuota - _randomRange, _requiredQuota + _randomRange);
-        _requiredQuota = random;
+        int baseQuota = 5000 * GameManager.Instance.StageManager.CurrentStage.Value;
+        int random = Random.Range(baseQuota - (int)(baseQuota * 0.1f), baseQuota + (int)(baseQuota * 0.1f));
+        RequiredQuota.Value = random;
+    }
+
+    public void SetCurrentQuotaToZero()
+    {
+        CurrentQuota.Value = 0;
     }
 
     public void AddCurrentQuota(int value)
     {
-        _currentQuota += value;
+        CurrentQuota.Value += value;
+        GameManager.Instance.GoldManager.AddGold(value);
+    }
 
-        RenewQuotaWhenStageChanged();
+    [ContextMenu("Test Add Gold")]
+    public void TestAddGold()
+    {
+        AddCurrentQuota(10000);
     }
 }
