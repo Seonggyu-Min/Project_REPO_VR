20f93bc208db566503f82a79f1e23c943f4e9900
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-17 17:13:34 +0900

    feat: 추가된 씬에 대응하는 씬로드 기능 추가

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Management;
 
 public class StageManager : MonoBehaviour
 {
@@ -11,8 +13,32 @@ public class StageManager : MonoBehaviour
         CurrentStage.Value = 1;
     }
 
+    [ContextMenu("Go To Shop")]
     public void GoToShopScene()
     {
+        CurrentStage.Value++;
+        SceneManager.LoadScene(1);
         Debug.Log("Go To Shop");
+    }
+
+    [ContextMenu("Go To Game")]
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Go To Game");
+    }
+
+    [ContextMenu("Restart Game")]
+    public void GoToGameOverScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoToGameSceneWithRestart()
+    {
+        CurrentStage.Value = 1;
+        GameManager.Instance.UpgradeManager.ResetPlayerStats();
+        GameManager.Instance.GoldManager.ResetGold();
+        SceneManager.LoadScene(0);
     }
 }
